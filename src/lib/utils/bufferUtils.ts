a4e3c802a4683bc4709484d0ad01c3d802701744
Anon-on-chain <sammchambertwo@gmail.com>
2025-01-22 16:24:24 +0000

    Delete src/lib/utils/bufferUtils.ts

--- src/lib/utils/bufferUtils.ts
+++
@@ -1,5 +0,0 @@
-// Apply buffer percentage to an amount
-export const applyBuffer = (amount: number, bufferPercent: number): number => {
-  if (bufferPercent <= 0) return amount;
-  return amount * (1 + bufferPercent / 100);
-};
